1922cc0740b3e6eca16af2fe3eec6b26680bb9c1
unknown <capitanmeseros2017@gmail.com>
2025-12-21 19:33:17 -0500

    Recuperé server.js y WhatsappButton.jsx

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -2,8 +2,8 @@ import { FaWhatsapp } from "react-icons/fa";
 import "../styles/WhatsappButton.css";
 
 const WhatsappButton = () => (
-  <a
-    href="https://wa.me/573151134822"
+  <a 
+    href="https://wa.me/573151138422"
     target="_blank"
     rel="noopener noreferrer"
     className="whatsapp"
